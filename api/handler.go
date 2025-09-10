95db0ef38b20ab3cd42c3309112921a51be2fe34
wildan011 <wildanardiawan@gmail.com>
2025-09-10 12:37:03 +0700

    fix: update vercel json

--- api/handler.go
+++ api/handler.go
@@ -2,12 +2,14 @@ package handler
 
 import (
 	"net/http"
+
 	"trello-services/internal/app"
 )
 
+// create the router once (cold start)
 var router = app.NewRouter()
 
-// Vercel entrypoint
+// Vercel entrypoint (must be exported "Handler")
 func Handler(w http.ResponseWriter, r *http.Request) {
 	router.ServeHTTP(w, r)
 }
